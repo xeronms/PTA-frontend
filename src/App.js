9453f701e6ce57bd9153ae7491f6f5b7dd6bb53d
Jakub <jakub.terkalski@gmail.com>
2021-12-04 17:38:49 +0100

    add homepage

--- src/App.js
+++ src/App.js
@@ -3,25 +3,24 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavbarComponent from './components/NavbarComponent';
 import { Container } from 'react-bootstrap';
 import MapContainer from './pages/MapContainer';
-import Home from './pages/MapContainer';
+import Home from './pages/Home';
 
 function App() {
-    return (
-        <Router>
-            <div className='overflow-hidden'>
-                <Container
-                    className='px-0'
-                    style={{ height: '95vh', minWidth: '100%', width: '100%' }}
-                >
-                    <NavbarComponent></NavbarComponent>
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/map' element={<MapContainer />} />
-                    </Routes>
-                </Container>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="overflow-hidden">
+        <Container
+          className="px-0"
+          style={{ height: '95vh', minWidth: '100%', width: '100%' }}>
+          <NavbarComponent></NavbarComponent>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/map" element={<MapContainer />} />
+          </Routes>
+        </Container>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+import './Home.css';
+
+const options = [
+  {
+    label: 'Group 1',
+    options: [
+      { value: 'p1', label: 'Przystanek1' },
+      { value: 'p2', label: 'Przystanek2' },
+      { value: 'p3', label: 'Przystanek3' },
+    ],
+  },
+
+  {
+    label: 'Group 2',
+    options: [
+      { value: 'p4', label: 'Przystanek4' },
+      { value: 'p5', label: 'Przystanek5' },
+      { value: 'p6', label: 'Przystanek6' },
+    ],
+  },
+  {
+    label: 'Group 3',
+    options: [
+      { value: 'p7', label: 'Przystanek7' },
+      { value: 'p8', label: 'Przystanek8' },
+    ],
+  },
+];
+
+const animatedComponents = makeAnimated();
 
 const Home = () => {
-    return <div>Hello start</div>;
+  const [from, setFrom] = React.useState([options[0], options[1]]);
+
+  const handleChange = (selectedOption) => {
+    setFrom((state) => [...state, selectedOption]);
+  };
+
+  return (
+    <div className="homeContainer">
+      <div className="selectbox">
+        <div className="label">Przystanek początkowy</div>
+        <Select
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          defaultValue={from}
+          isMulti
+          options={options}
+          onChange={handleChange}
+          options={options}
+        />
+      </div>
+      <button class="nextButton">Dalej</button>
+    </div>
+  );
 };
 
 export default Home;
