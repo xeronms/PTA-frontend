8ac082b14be5fef0dc200da48703ee8a8fc68e0f
Michał P <michal@MacBook-Pro-Micha.local>
2021-12-05 02:03:29 +0100

    fun1 complete

--- src/helpers/Backend.js
+++ src/helpers/Backend.js
@@ -1,16 +1,9 @@
-import * as dfd from "danfojs/dist/index";
-
 const Backend = () => {
-    const ridesDbFilename = 'Dane_Ostateczne_1.csv';
-    //const busStopsDbFilename = 'Dane_Ostateczne_1.csv';
-
-    // const ridesDbFilename = 'Przejazdy.csv';
-    // const busStopsDbFilename = 'Przystanki.csv';
+    const ridesDbFilename = 'Dane_Ostateczne_1_transformed.csv';
 
     const onClickHandler = async () => {
         //try {
-        const ridesDf = await getDataFromCsv(ridesDbFilename);
-        //const busStopsDf = await getDataFromCsv(busStopsDbFilename);
+        const ridesTable = await getDataFromCsv(ridesDbFilename);
 
         // *** hard-codowane dane --> docelowo otrzymuję to z frontu *** 
         // const busStopsGroupIds1 = [
@@ -37,16 +30,14 @@ const Backend = () => {
             'ch osiedle',
             'mn księżok',
         ];
-        const dateFrom = '2021-09-01';
-        const dateTo = '2021-09-03';
-        const hourFrom = '01:00:00';
-        const hourTo = '23:00:00';
-
+        const dateFrom = 1630447200; // 2021-09-01
+        const dateTo = 1630620000;   // 2021-09-03
+        const hourFrom = '100';      // 01:00:00
+        const hourTo = '2300';       // 23:00:00
 
         // Funkcjonalność #1
         fun1(
-            ridesDf,
-            //busStopsDf,
+            ridesTable,
             busStopsGroupIds1,
             dateFrom,
             dateTo,
@@ -57,7 +48,6 @@ const Backend = () => {
         // Funkcjonalność #2
         // fun2(
         //     ridesDf,
-        //     //busStopsDf,
         //     busStopsGroupIds1,
         //     busStopsGroupIds2,
         //     dateFrom,
@@ -72,49 +62,45 @@ const Backend = () => {
 
     // ============== FUNKCJONALNOŚĆ #1 ==============
     function fun1(
-        ridesDf,
-        //busStopsDf,
-        stopsGroupIds,
+        ridesTable,
+        busStopsGroupIds,
         dateFrom,
         dateTo,
         hourFrom,
         hourTo
     ) {
-        // 1. tmp = SELECT * FROM tmp WHERE data > dateFrom AND data < dateTo;
-        // 2. tmp = SELECT * FROM tmp WHERE Godzina > hourFrom AND Godzina < hourTo;
-        // TODO #1: kolumna Godzina -> usunąć : i zmienić na inta (musi być skalar by porównywać)
-        // TODO #2: kolumna data -> zmienić na czasu Unixowy, wtedy otrzebujemy skalara 
-        ridesDf.print();
-        ridesDf.print();
+        let ridesTableLines = ridesTable.split('\n');
+        let dateColumnIdx = 2
+        let hourColumnIdx = 4
+        let busStopFromIdColumnIdx = 5
 
-        ridesDf["data"] = new dfd.Series(ridesDf["data"])
+        // 1. Zastosowanie ograniczeń czasowych
+        let rowsWithinTimeLimit = []
+        for (let i = 0; i < ridesTableLines.length; i++) {
+            let colVals = ridesTableLines[i].split(',');
+            if (colVals[dateColumnIdx] >= dateFrom &&
+                colVals[dateColumnIdx] <= dateTo &&
+                colVals[hourColumnIdx] >= hourFrom &&
+                colVals[hourColumnIdx] <= hourTo) {
+                rowsWithinTimeLimit.push(colVals);
+            }
+        }
+        if (rowsWithinTimeLimit.length == 0) {
+            throw "No records found for given time boundaries!";
+        }
 
-        // let ridesDfTimeFiltered = ridesDf.iloc({
-        //     rows: ridesDf["data"].ge(dateFrom)
-        //         .and(ridesDf["data"].le(dateTo))
-        //         .and(ridesDf["Godzina"].ge(hourFrom))
-        //         .and(ridesDf["Godzina"].le(hourTo))
-        // });
-        let ridesDfTimeFiltered = ridesDf.iloc({
-            rows: ridesDf["Godzina"].gt(hourFrom)
-        });
-        ridesDfTimeFiltered.print();
-
-        // 3. tmp = SELECT * FROM ridesDf WHERE przystanek_początkowy_id IN stopsGroupIds;
-        // let ridesDfTimeAndIdFilteredArr = [];
-        // for (let stopId in stopsGroupIds) {
-        //     let ridesDfTimeAndIdFiltered = ridesDfTimeFiltered.iloc({
-        //         rows: ridesDfTimeFiltered["Przystanek_Początkowy_id"].eq(stopId)
-        //     });
-        //     ridesDfTimeAndIdFilteredArr.push(ridesDfTimeAndIdFiltered);
-        // }
-
-        // 4. return SELECT COUNT(*) FROM tmp
-        // let countsPerBusStopArr = [];
-        // for (let ridesDfTimeAndIdFiltered in ridesDfTimeAndIdFilteredArr) {
-        //     let counted = ridesDfTimeAndIdFiltered.count();
-        //     // TODO: obczaić jak to zliczać dokładnie XD ew. df.shape
-        // }
+        // 2. Zliczenie wystąpień dla kazdego z przystankow
+        let busStopsOccurencesCount = {}
+        busStopsGroupIds.forEach(item => busStopsOccurencesCount[item] = 0)
+        for (let busStopId of busStopsGroupIds) {
+            for (let row of rowsWithinTimeLimit) {
+                if (row[busStopFromIdColumnIdx] === busStopId) {
+                    busStopsOccurencesCount[busStopId]++;
+                }
+            }
+        }
+        console.log(busStopsOccurencesCount)
+        return busStopsOccurencesCount;
     }
 
     // ============== FUNKCJONALNOŚĆ #2 ==============
@@ -131,22 +117,16 @@ const Backend = () => {
         // 1. tmp = SELECT * FROM tmp WHERE data > dateFrom AND data < dateTo;
         // 2. tmp = SELECT * FROM tmp WHERE Godzina > hourFrom AND Godzina < hourTo;
 
-        let ridesDfTimeFiltered = ridesDf.iloc({
-            rows: ridesDf["data"].ge(dateFrom)
-                .and(ridesDf["data"].le(dateTo))
-                .and(ridesDf["Godzina"].ge(hourFrom))
-                .and(ridesDf["Godzina"].le(hourTo))
-        });
-
         // 3. foreach fromId in stopsFromIds: 
         //      foreach toId in stopsToIds:
         //        SELECT COUNT(*) FROM ridesDfTimeFiltered WHERE przystanek_początkowy_id=fromId AND przystanek_koncowy_id=toId
-        let countsBusStopConnectionsArr = []
+
     }
 
     // ============== FUNKCJE POMOCNICZE ==============
     const getDataFromCsv = async (filename) => {
-        return dfd.read_csv(filename);
+        const res = await fetch(filename);
+        return await res.text();
     };
 
     return (
